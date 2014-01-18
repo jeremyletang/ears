0f15e1a2af69cd24719bb908a5f593aa4c028176
JeremyLetang <letang.jeremy@gmail.com>
2014-01-17 22:52:51 -0500

    fix tests

--- src/ears/music.rs
+++ src/ears/music.rs
@@ -644,7 +644,7 @@ impl Drop for Music {
 #[cfg(test)]
 mod test {
     use music::Music;
-    use states::{State, Inital, Playing, Paused, Stopped};
+    use states::{State, Initial, Playing, Paused, Stopped};
     use audio_controller::AudioController;
 
     #[test]
