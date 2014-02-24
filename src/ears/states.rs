0f93e6308dbefa83d6502fd7a56baac5c4309c92
JeremyLetang <letang.jeremy@gmail.com>
2014-02-24 10:30:02 -0500

    update to last rust
    - ToStr -> Show

--- src/ears/states.rs
+++ src/ears/states.rs
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2013 Jeremy Letang (letang.jeremy@gmail.com)
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
 // the Software without restriction, including without limitation the rights to
 // use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 // the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 // FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -19,12 +19,10 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-/*!
- * The states of a Sound or a Music.
- */
+/*! The states of a Sound or a Music. */
 
 /// The differents states in which a sound can be.
-#[deriving(Clone, Eq, Ord, ToStr)]
+#[deriving(Clone, Eq, Ord, Show)]
 pub enum State {
     /// Initial state of the sound or music
     Initial,
